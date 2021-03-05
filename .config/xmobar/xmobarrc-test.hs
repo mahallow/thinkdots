86aec8d7089c824926dd6c1188f142c4f4929847
Liam <heinicke@seattleu.edu>
2021-03-05 09:28:35 -0800

    xmobar with many a functions all working

--- .config/xmobar/xmobarrc-test.hs
+++ .config/xmobar/xmobarrc-test.hs
@@ -6,9 +6,6 @@ font = "xft:CozetteVector-9",
 bgColor = "black",
 fgColor = "grey",
 position = TopW L 100,
-
--- list of commands which gather information about your system for
--- presentation in the bar.
 commands = [
 	-- Gather and format CPU usage information.
 	-- If it's above 70%, we consider it high usage and make it red.
@@ -23,7 +20,6 @@ commands = [
 	Run Network "enp0s31f6" ["-L","0","-H","32","--normal","green","--high","red"] 10,
 	Run Network "wlp58s0" ["-L","0","-H","32","--normal","green","--high","red"] 10,
 
-	--Run Brightness ["-t", ""] 60,
 
 	-- Gather and format memory usage information
 	Run Memory [
@@ -46,19 +42,12 @@ commands = [
 	"-h", "green",
 	"-l", "red"
 	] 100,
-	-- To get volume information, we run a custom bash script.
-	-- This is because the built-in volume support in xmobar is disabled
-	-- in Debian and derivatives like Ubuntu.
-	--Run Com "~/bin/get-volume.sh" [] "myvolume" 10,
-	-- /!\ https://github.com/jaor/xmobar/issues/127
-	--Run Com "/bin/bash" ["-c", "~/.xmonad/get-volume.sh"]  "myvolume" 1,
-	Run Com "amixer" ["-R | grep 'Front Left: Playback'" ]  "myvolume" 1,
 
-
-	--Run Com "/bin/bash" ["-c", "echo `xbacklight -get | grep -oE '^.[0-9]{0,3}'`%"]  "mybright" 1,
-	Run Com "brightnessctl" ["-d", "intel_backlight", "g"]  "mybright" 600,
-	--Run Com "~/bin/get-volume.sh" [] "vol",
-	--Run Weather "KNYC" ["-t", "KNYC: <tempC>C/<skyCondition>"] 36000,
+	Run Com "echo" ["swag"] "note" 0,
+	Run Com "volume" [""] "vol" 10,
+	Run Com "uname" ["-r"] "kernal" 0,
+	--Brightness
+	Run Com "brightnessctl" ["-d", "intel_backlight", "g"]  "mybright" 60,
 	-- This line tells xmobar to read input from stdin. That's how we
 	-- get the information that xmonad is sending it for display.
 	Run StdinReader
@@ -73,6 +62,6 @@ alignSep = "}{",
 
 -- Overall template for the layout of the xmobar contents. Note that
 -- space is significant and can be used to add padding.
-template = "| %StdinReader% }{ %enp0s31f6%%wlp58s0% | %battery% | %cpu% | %memory% | %myvolume% | Lum: %mybright% | <fc=#e6744c>%date%</fc> | "
+template = "| %StdinReader% }{ %enp0s31f6%%wlp58s0% | %battery% | %cpu% | %memory% | Volume: %vol% | Lum: %mybright% | <fc=#e6744c>%date%</fc> | %kernal% | %note% "
 }
 
