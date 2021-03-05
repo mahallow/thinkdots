15948b4e5d7f69f8475ca965d4ed4ad8d7d66f59
Liam <heinicke@seattleu.edu>
2021-03-05 08:48:50 -0800

    lockscreen with blur!

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -151,7 +151,7 @@ myKeys =
      , ("M-g", sendMessage $ JumpToLayout "Grid")
      , ("M-b", sendMessage $ JumpToLayout "BSP")
      , ("M-,", spawn "rofi -show drun") -- rofi
-     , ("M-S-l", spawn "i3lock -c 111111") -- rofi
+     , ("M-S-l", spawn "i3lock-fancy-rapid 8 3") -- lockscreen
      , ("<XF86MonBrightnessUp>", spawn "brightnessctl -d intel_backlight set +300") -- rofi
      , ("<XF86MonBrightnessDown>", spawn "brightnessctl -d intel_backlight set 300-") -- rofi
      , ("<XF86AudioLowerVolume>", spawn "amixer set Master 5%- unmute") -- rofi
