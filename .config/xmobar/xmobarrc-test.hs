2fe1a1916bc7ccb795f6b8b4c2206a04b3472165
Liam <heinicke@seattleu.edu>
2021-03-04 19:43:47 -0800

    xmobar config clean up

--- .config/xmobar/xmobarrc-test.hs
+++ .config/xmobar/xmobarrc-test.hs
@@ -1,14 +1,3 @@
--- xmobar configuration file
---
--- Mess with this file to change the appearance of the contents of the
--- status bar which are not directly controlled by xmonad. You can see your
--- changes immediately by recompiling xmonad using "super-q".
---
--- There's a lot you can do here. Refer to "man xmobar".
---
--- Author: David Brewer
--- Repository: https://github.com/davidbrewer/xmonad-ubuntu-conf
-
 
 Config {
 -- Set font and default foreground/background colors. Note that
@@ -16,12 +5,6 @@ Config {
 font = "xft:CozetteVector-9",
 bgColor = "black",
 fgColor = "grey",
-
--- Position is top left, taking up 95% of screen.
--- You are likely to have to tweak the width here based on the width
--- of your screen to make it play nicely with stalonetray, which we
--- want to be taking up the remainer of the space on the right side
--- of your screen.
 position = TopW L 100,
 
 -- list of commands which gather information about your system for
@@ -73,14 +56,7 @@ commands = [
 
 	Run Com "/bin/bash" ["-c", "echo `xbacklight -get | grep -oE '^.[0-9]{0,3}'`%"]  "mybright" 1,
 	--Run Com "~/bin/get-volume.sh" [] "vol",
-
-	--Run Com "~/bin/babbage-latency" [] "babbageLatency" 10,
-	--Run Com "~/bin/google-latency" [] "googleLatency" 10,
-	--Run Com "~/bin/hs0-latency" [] "hs0Latency" 10,
-
 	--Run Weather "KNYC" ["-t", "KNYC: <tempC>C/<skyCondition>"] 36000,
-
-	--Run Network "eth0" [] 36000,
 	-- This line tells xmobar to read input from stdin. That's how we
 	-- get the information that xmonad is sending it for display.
 	Run StdinReader
