344da569649e6fdfab7f59aabf8bd93276008547
Liam <heinicke@seattleu.edu>
2021-03-06 20:40:03 -0800

    configuration.nix organization

--- configuration.nix
+++ configuration.nix
@@ -84,70 +84,66 @@
         persist = true;
      }];
 
-  # Enable AwesomeWM
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm = {
+  # Compositor and session manager 
+    services.picom = {
+       enable = true;
+       fade = true; 
+       inactiveOpacity = 0.9;
+       shadow = true;
+       fadeDelta = 1; 
+       vSync = true;
+      };
+    services.xserver.enable = true;
+    services.xserver.displayManager.gdm = {
 	enable = true;
 	wayland = false;
 	};
-  services.xserver.windowManager.awesome.enable = true;
-  services.picom = {
-    enable = true;
-    fade = true; 
-    inactiveOpacity = 0.9;
-    shadow = true;
-    fadeDelta = 1; 
-    vSync = true;
-    };
 
   # Enable XMonad
-  services.xserver.windowManager.xmonad = {
+    services.xserver.windowManager.xmonad = {
 	enable = true;
 	extraPackages = hpkgs: [
    	  hpkgs.xmonad
 	  hpkgs.xmonad-contrib
 	  hpkgs.xmonad-extras
 	  ];
-  };
+      };
 
 
   # Set Fish as shell
    programs.fish.enable = true;
    users.users.liam = {
    shell = pkgs.fish;
-  };
+   };
 
   # List packages installed in system profile.
+    nixpkgs.config.allowUnfree = true;
     environment.systemPackages = with pkgs; [
     wget
     vim
-    # Window Management
+   # Window Management
     rofi
     autorandr
-    dmenu
-    i3lock
     pkgs.lxappearance
-    polybar
     xmobar
-    # System Applications
+   # System Applications
     pkgs.kitty
     pkgs.python39
     gotop
     pkgs.arandr
     pciutils
     brightnessctl
-    # File management
+   # File management
     pkgs.ranger
     pkgs.gnome3.nautilus
     pkgs.gitAndTools.gitFull
     pkgs.zathura
     unzip
     feh
-    # Misc
+   # Misc
     scrot
     pfetch
-  ];
-     nixpkgs.config.allowUnfree = true;
+   ];
  
  # Fonts
   fonts.fonts = with pkgs; [
@@ -158,11 +154,12 @@
  # Use most up to date Linux Kernals 
     boot.kernelPackages = pkgs.linuxPackages_latest;
 
-# NixOS Configuration 
+ # NixOS Configuration 
   system.stateVersion = "20.09"; # Did you read the comment?
   #system.autoUpgrade = {
   #     enable = true;
   #     channel = https://nixos.org/channels/nixos-unstable;
   #     };
+
 }
 
