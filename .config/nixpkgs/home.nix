5bfab744106f91951f8e238dc2c97ec848a8b6c8
Liam <heinicke@seattleu.edu>
2021-03-29 09:19:35 -0700

    slowing adjusting to this home-manager business with nvim

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -3,10 +3,38 @@
 {
 programs.home-manager.enable = true;
 programs.bat.enable = true;
+programs.neovim = {
+	enable = true;
+        extraConfig = ''
+            set t_Co=256
+            set paste
+            set number relativenumber
+            colorscheme gruvbox
+            set ignorecase
+            set smartcase
+            set mouse=a
+            set noswapfile
+            let NERDTreeShowHidden = 1
+            map<F7> :NERDTree <CR>
+          '';
+
+	plugins = with pkgs.vimPlugins; [
+	vim-nix
+	vim-pandoc
+        multiple-cursors
+	gruvbox
+	vim-airline
+	vim-surround
+	nerdtree
+	];
+  };
+
+home.sessionVariables = {
+	EDITOR = "vim";
+  };
 
 home.packages = with pkgs; [
    #system 
-	cmus
 	pulsemixer
 	sutils
 	qemu
@@ -15,12 +43,18 @@ home.packages = with pkgs; [
 	brave
 	qutebrowser
 	nyxt
+   #TUI Utils
 	youtube-dl
+	pywal
+	fontpreview
+	cmus
 	texlive.combined.scheme-full
 	i3lock-fancy-rapid
 	mpv-with-scripts
 	sc-im
 	pandoc
+   #Other
+	obs-studio
   ];
 
 }
