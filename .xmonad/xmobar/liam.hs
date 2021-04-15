7025481cb41edc29dd0962cc5700ead9ad31e73e
Liam <heinicke@seattleu.edu>
2021-04-15 12:55:22 -0700

    colorchange

--- .xmonad/xmobar/liam.hs
+++ .xmonad/xmobar/liam.hs
@@ -64,6 +64,6 @@ alignSep = "}{",
 
 -- Overall template for the layout of the xmobar contents. Note that
 -- space is significant and can be used to add padding.
-template = " | %StdinReader% }{ %enp0s31f6%%wlp58s0% | <fc=#c5a900>%battery%</fc> | %cpu% | %memory% | Vol: %vol% | <fc=#98971a>%disku%</fc> | Lum: %mybright% | <fc=#e6744c>%date%</fc> | %kernal% | %note% | "
+template = " | %StdinReader% }{ %enp0s31f6%%wlp58s0% | <fc=#c5a900>%battery%</fc> | %cpu% | %memory% | Vol: %vol% | <fc=#458588>%disku%</fc> | Lum: %mybright% | <fc=#e6744c>%date%</fc> | %kernal% | %note% | "
 }
 
